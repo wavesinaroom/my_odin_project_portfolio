20bde0552c1d4211b4f3fc92d34874923ee9cb6b
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-20 15:07:48 -0500

    [Feature] Mouse enter - Mouse leave events

--- interaction.js
+++ interaction.js
@@ -2,12 +2,24 @@ const projectInteractives = Array.from(document.getElementsByClassName(`project-
 const dialogs = Array.from(document.getElementsByTagName(`dialog`));
 
 projectInteractives.forEach((interactive)=>{
-  interactive.addEventListener(`click`, ()=>{
-    dialogs.forEach((d)=>{
-      d.open = false;
-    });
+  interactive.children[0].addEventListener(`mouseenter`, ()=>{
     const dialog = interactive.children[1];
     dialog.open = true;
   });
+
+  interactive.children[0].addEventListener(`mouseleave`, ()=>{
+    dialogs.forEach((d)=>{
+      d.open = false;
+    });
+  })
 });
 
+dialogs.forEach((d)=>{
+  d.addEventListener(`mouseenter`, ()=>{
+    d.open = true;
+  });
+  
+  d.addEventListener(`mouseleave`, ()=>{
+    d.open = false;
+  });
+})
