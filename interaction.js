58cd2a72f664998b2228f06ddd4207fe8c684603
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-29 15:13:57 -0500

    [Feature] Pop up panels on button click

--- interaction.js
+++ interaction.js
@@ -1,6 +1,8 @@
 const projectInteractives = Array.from(document.getElementsByClassName(`project-interactive`));
 const dialogs = Array.from(document.getElementsByTagName(`dialog`));
 const details = Array.from(document.getElementsByTagName(`details`));
+const panels = Array.from(document.getElementsByClassName("panel"));
+const navButtons = Array.from(document.querySelectorAll(`#nav-buttons > button`));
 
 projectInteractives.forEach((interactive)=>{
   interactive.children[0].addEventListener(`mouseenter`, ()=>{
@@ -12,7 +14,7 @@ projectInteractives.forEach((interactive)=>{
     dialogs.forEach((d)=>{
       d.open = false;
     });
-  })
+  });
 });
 
 dialogs.forEach((d)=>{
@@ -24,14 +26,24 @@ dialogs.forEach((d)=>{
   d.addEventListener(`mouseleave`, ()=>{
     d.open = false;
   });
-})
+});
 
 details.forEach((interactive)=>{
   interactive.addEventListener(`mouseenter`, ()=>{
     interactive.open = true;
-  })
+  });
 
   interactive.addEventListener(`mouseleave`, ()=>{
     interactive.open = false;
-  })
-})
+  });
+});
+
+navButtons.forEach((button)=>{
+  const panel = document.getElementById(button.id.slice(3));
+  button.addEventListener(`mousedown`, ()=>{
+    panel.style.display = `contents`;
+    panel.scrollIntoView({behavior:`smooth`});
+    document.getElementById(`center`).style.display = `none`;
+  });
+});
+
